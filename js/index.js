82bb8880ebe4a2826484ac95c38756bd0c1537d8
Ayoub Jabiri <ayoubjabiri.coding@gmail.com>
2025-12-30 17:59:36 +0100

    Added authentication page design

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// 1. <-- Main Selectors -->
+
+let adminRoleBtn = document.getElementById("admin-btn"),
+    learnerRoleBtn = document.getElementById("learner-btn"),
+    adminPage = document.getElementById("admin-page"),
+    learnerPage = document.getElementById("learner-page");
+
+// 2. <-- Toggle role -->
